445f27952066ae0a255883b3503236b96eb1c927
Parva Jain <parv@Parvas-MacBook-Air.local>
2022-02-01 13:44:03 +0530

    burn n batchMint added

--- src/nft/mint.js
+++ src/nft/mint.js
@@ -2,7 +2,6 @@ const UTILS = require("../common/utils");
 const { PNDC_ABI } = require("../../abi/pndc");
 
 const mint = async (web3, chainId, minterAddress, tokenURI, royalties) => {
-
   const PNDC_instance = await UTILS.PNDC_instance(web3, chainId, PNDC_ABI);
 
   let result = await PNDC_instance.methods
@@ -13,4 +12,33 @@ const mint = async (web3, chainId, minterAddress, tokenURI, royalties) => {
   return result;
 };
 
-module.exports = { mint };
+const batchMint = async (
+  web3,
+  chainId,
+  minterAddress,
+  totalNFT,
+  uriArray,
+  royalties
+) => {
+  const PNDC_instance = await UTILS.PNDC_instance(web3, chainId, PNDC_ABI);
+
+  let result = await PNDC_instance.methods
+    .batchMint(totalNFT, uriArray, royalties)
+    .send({ from: minterAddress });
+
+  console.log(result);
+  return result;
+};
+
+const burn = async (web3, chainId, minterAddress, tokenId) => {
+  const PNDC_instance = await UTILS.PNDC_instance(web3, chainId, PNDC_ABI);
+
+  let result = await PNDC_instance.methods
+    .burn(tokenId)
+    .send({ from: minterAddress });
+
+  console.log(result);
+  return result;
+};
+
+module.exports = { mint, batchMint, burn };
